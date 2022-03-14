09947c7772c8275cf331ede1be53d3e11a01043b
professorisidro <isidro@professorisidro.com.br>
2022-03-14 14:25:30 -0300

    query by method name

--- src/main/java/br/com/isidrocorp/ecommerce/controller/DepartamentoController.java
+++ src/main/java/br/com/isidrocorp/ecommerce/controller/DepartamentoController.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.isidrocorp.ecommerce.dao.DepartamentoDAO;
@@ -21,6 +24,16 @@ public class DepartamentoController {
 		return (List<Departamento>)dao.findAll();
 	}
 	
+	
+	@PostMapping("/departamentos")
+	public Departamento incluirNovo(@RequestBody Departamento novo) {
+		return dao.save(novo);
+	}
+	
+	@PutMapping("/departamentos")
+	public Departamento alterarDepartamento(@RequestBody Departamento atual) {
+		return dao.save(atual);
+	}
 	
 
 }
--- src/main/java/br/com/isidrocorp/ecommerce/dao/ProdutoDAO.java
+++ src/main/java/br/com/isidrocorp/ecommerce/dao/ProdutoDAO.java
@@ -1,11 +1,22 @@
 package br.com.isidrocorp.ecommerce.dao;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
+import br.com.isidrocorp.ecommerce.model.Departamento;
 import br.com.isidrocorp.ecommerce.model.Produto;
 
 public interface ProdutoDAO extends CrudRepository<Produto, Integer>{
 
 	//	aqui virão as minhas consultas customizadas
+	// quero uma busca que traga produtos por palavra chave
+	
+	public List<Produto> findByNomeContainingOrDescricaoContaining(String pcNome, String pcDescricao);
+	public List<Produto> findByOrderByNomeAsc();
+	public List<Produto> findByOrderByPrecoAsc();
+	public List<Produto> findByOrderByPrecoDesc();
+	public List<Produto> findByDepto(Departamento d);
 
 }
+
