7e6e11656a18c9ecec5d838a2dbdd94660de7f75
professorisidro <isidro@professorisidro.com.br>
2022-03-17 09:54:02 -0300

    usuario service

--- src/main/java/br/com/isidrocorp/ecommerce/services/UsuarioServiceImpl.java
+++ src/main/java/br/com/isidrocorp/ecommerce/services/UsuarioServiceImpl.java
@@ -1,5 +1,22 @@
 package br.com.isidrocorp.ecommerce.services;
 
-public class UsuarioServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import br.com.isidrocorp.ecommerce.dao.UsuarioDAO;
+import br.com.isidrocorp.ecommerce.model.Usuario;
+import br.com.isidrocorp.ecommerce.security.Token;
+
+@Component
+public class UsuarioServiceImpl implements IUsuarioService{
+
+	@Autowired
+	private UsuarioDAO dao; 
+	
+	@Override
+	public Token gerarTokenDeUsuarioLogado(Usuario dadosLogin) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
